7076bce7cafb40b79fd3d4eadccea682331bbb00
Irosh Mayantha <Irosh Mayantha@LAPTOP-E4SAT463>
2020-02-29 11:20:18 +0530

    Initial commit. Created a spring boot project.

---
+++ Inventory Management System/src/main/java/in/irosh/Inventory/Management/System/InventoryManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package in.irosh.Inventory.Management.System;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryManagementSystemApplication.class, args);
+	}
+
+}
